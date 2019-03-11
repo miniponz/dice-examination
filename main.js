182ebbcbdbd6fa966b318f0700de2502c1a7ea51
Cara Ponzini <ponzini@gmail.com>
2019-03-10 21:43:33 -0700

    added input field, form and button to set high score

--- main.js
+++ main.js
@@ -36,6 +36,8 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 });
 
 
+
+
 document.querySelector('.btn-hold').addEventListener('click', function() {
     if (gamePlaying) {
         // Add CURRENT score to GLOBAL score
